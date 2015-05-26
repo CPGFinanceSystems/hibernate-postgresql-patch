cf51d573c78512769c24d5e1448da049c6758e91
Forstner Michael <michael.forstner@cpg.de>
2015-05-26 15:56:18 +0200

    Add custom PostgreSQL dialect with native UUID type support

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hibernate-postgresql-patch
 Provides a custom extended PostgreSQL dialect for Hibernate 4.x
+
+Currently it constists only of a fix for mapping the native `java.util.UUID` to the `pg-uuid` type without adding
+JPA's `@Type` annotation at entity field level.
+
+## Usage
+
+Use `de.cpg.shared.hibernate.CustomPostgreSQL9Dialect` instead of standard `org.hibernate.dialect.PostgreSQL9Dialect`
+in your application's hibernate configuration an you're done.
+
+## Download
+
+Binaries are available on maven central with following GAV:
+
+- groupId: `de.cpg.shared`
+- artifactId: `hibernate-postgresql-patch`
+- version: `1.0.0`
---
+++ src/main/java/de/cpg/shared/hibernate/CustomPostgreSQL9Dialect.java
@@ -0,0 +1,30 @@
+package de.cpg.shared.hibernate;
+
+import org.hibernate.dialect.PostgreSQL9Dialect;
+import org.hibernate.metamodel.spi.TypeContributions;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.type.PostgresUUIDType;
+
+/**
+ * Custom dialect for Hibernate and PostgreSQL so java.util.UUID is mapped to pg-uuid automatically without @Type()
+ * annotation at entity field level.
+ * Furthermore if i.e. the H2 DB is used for testing, it not know anything about pg-uuid. The mapping of java.util.UUID
+ * in Hibernate with H2 DB just works as expected.
+ */
+public class CustomPostgreSQL9Dialect extends PostgreSQL9Dialect {
+
+    @Override
+    public void contributeTypes(final TypeContributions typeContributions, final ServiceRegistry serviceRegistry) {
+        super.contributeTypes(typeContributions, serviceRegistry);
+        typeContributions.contributeType(new InternalPostgresUUIDType());
+    }
+
+    protected static class InternalPostgresUUIDType extends PostgresUUIDType {
+
+        @Override
+        protected boolean registerUnderJavaType() {
+            return true;
+        }
+    }
+}
+
